9ed01310a7bd7b8c6f5c9862ec9825fc0383f540
salmanA169 <40442174+salmanA169@users.noreply.github.com>
2021-08-16 16:46:26 +0300

    add new Activity

---
+++ app/src/main/java/com/example/myapplication/NewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
